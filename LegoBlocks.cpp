7b42ca3aa8e237b51ecfc14538fee201bb231ce1
Mehmet Arda Muftuoglu <marda@sabanciuniv.edu>
2022-09-16 20:11:16 +0300

    Worked on case 0 and case 10, time limit problems and abort problems in other cases

--- LegoBlocks.cpp
+++ LegoBlocks.cpp
@@ -16,16 +16,88 @@ vector<string> split(const string &);
  *  2. INTEGER m
  */
 
-void findWalls(vector<string>& wallArr, string& wall, int m, int curr)
+void findWalls(vector<string> &wallArr, string &wall, const int m, const int curr)
 {
-    
+    if (curr == m)
+    {
+        wallArr.push_back(wall);
+    }
+    else
+    {
+        if (curr + 1 <= m)
+        {
+            string newWall = wall + to_string(curr + 1);
+            findWalls(wallArr, newWall, m, curr + 1);
+
+            if (curr + 2 <= m)
+            {
+                newWall = wall + to_string(curr + 2);
+                findWalls(wallArr, newWall, m, curr + 2);
+
+                if (curr + 3 <= m)
+                {
+                    newWall = wall + to_string(curr + 3);
+                    findWalls(wallArr, newWall, m, curr + 3);
+
+                    if (curr + 4 <= m)
+                    {
+                        newWall = wall + to_string(curr + 4);
+                        findWalls(wallArr, newWall, m, curr + 4);
+                    }
+                }
+            }
+        }
+    }
 }
 
-
-int legoBlocks(int n, int m) {
-    
+void findHeight(const vector<string> &wallArr, vector<int> &crackNum, int n, int height, int &currNum, string currWall, int currIndex)
+{
+    if (height == n)
+    {
+        bool perfect = true;
+        for (int index = 0; perfect && index < crackNum.size() - 1; index++)
+        {
+            if (crackNum[index] == n)
+                perfect = false;
+        }
+        if (perfect)
+            currNum++;
+    }
+    else
+    {
+        for (int index = 0; index < wallArr.size(); index++)
+        {
+            string wall = wallArr[index];
+            vector<int> newCracks = crackNum;
+            for (int indexCrack = 0; indexCrack < wall.length(); indexCrack++)
+            {
+                newCracks[wall.at(indexCrack) - '0' - 1]++;
+            }
+            findHeight(wallArr, newCracks, n, height + 1, currNum, wall, index);
+        }
+    }
 }
-    
+
+int legoBlocks(int n, int m)
+{
+    string wall = "";
+    vector<string> wallArr;
+    findWalls(wallArr, wall, m, 0);
+
+    int currNum = 0;
+
+    for (int index = 0; index < wallArr.size(); index++)
+    {
+        vector<int> crackNum(m, 0);
+        for(int indexCrack = 0; indexCrack < wallArr[index].length(); indexCrack++)
+        {
+            crackNum[wallArr[index].at(indexCrack) - '0' - 1]++;
+        }
+        findHeight(wallArr, crackNum, n, 1, currNum, wallArr[index], index);
+    }
+
+    return currNum;
+}
 
 int main()
 {
@@ -36,7 +108,8 @@ int main()
 
     int t = stoi(ltrim(rtrim(t_temp)));
 
-    for (int t_itr = 0; t_itr < t; t_itr++) {
+    for (int t_itr = 0; t_itr < t; t_itr++)
+    {
         string first_multiple_input_temp;
         getline(cin, first_multiple_input_temp);
 
@@ -56,35 +129,37 @@ int main()
     return 0;
 }
 
-string ltrim(const string &str) {
+string ltrim(const string &str)
+{
     string s(str);
 
     s.erase(
         s.begin(),
-        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
-    );
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace))));
 
     return s;
 }
 
-string rtrim(const string &str) {
+string rtrim(const string &str)
+{
     string s(str);
 
     s.erase(
         find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
-        s.end()
-    );
+        s.end());
 
     return s;
 }
 
-vector<string> split(const string &str) {
+vector<string> split(const string &str)
+{
     vector<string> tokens;
 
     string::size_type start = 0;
     string::size_type end = 0;
 
-    while ((end = str.find(" ", start)) != string::npos) {
+    while ((end = str.find(" ", start)) != string::npos)
+    {
         tokens.push_back(str.substr(start, end - start));
 
         start = end + 1;
