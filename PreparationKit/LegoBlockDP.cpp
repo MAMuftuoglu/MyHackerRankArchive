a0cc97a430f5da0c5d55dedb69c6ae61834976ef
Mehmet Arda Muftuoglu <arda.muftuoglu@wur.nl>
2023-04-19 12:22:18 +0200

    To avoid overflow implemented addition and multiplication with mod

--- PreparationKit/LegoBlockDP.cpp
+++ PreparationKit/LegoBlockDP.cpp
@@ -1,15 +1,32 @@
 #include <iostream>
 #include <cmath>
 
+unsigned long addMod(unsigned long a, unsigned long b, unsigned long mod) {
+    return (a < (mod - b)) ? a + b : a - (mod - b);
+}
+
+unsigned long multiplyMod(unsigned long a, unsigned long b, unsigned long mod) {
+    unsigned int result = 0;
+
+    while(b > 0){
+        if(b & 1) result = addMod(result, a, mod);
+        a = addMod(a, a, mod);
+        b >>= 1;
+    }
+
+    return result;
+}
+
 int legoBlocks(int n, int m) {
-    int * rowArr = new int[m];
+    unsigned long * rowArr = new unsigned long[m];
     rowArr[0] = 1;
     int indexSum;
+    unsigned long mod = std::pow(10, 9) + 7;
     
     for(int index = 1; index < m; index++) {
         indexSum = 0;
         for(int blockSize = 1; blockSize <=4 && (index - blockSize) >= -1; blockSize++) {
-            if(index - blockSize == -1) indexSum++;
+            if(index - blockSize == -1) addMod(indexSum, 1, mod);
             else if (index - blockSize > -1) indexSum += rowArr[index - blockSize];
         }
         
@@ -17,7 +34,7 @@ int legoBlocks(int n, int m) {
     }
     
     
-    int * resultArr = new int[m];
+    unsigned long * resultArr = new unsigned long[m];
     resultArr[0] = 1;
     
     for(int index = 1; index < m; index++) {
@@ -26,12 +43,10 @@ int legoBlocks(int n, int m) {
             indexSum -= resultArr[leftCut] * std::pow(rowArr[index - leftCut - 1], n);
         }
         resultArr[index] = indexSum;
-        std::cout << indexSum << " ";
+        // std::cout << indexSum << " ";
     }
     
-    std::cout << std::endl;
-
-    int mod = std::pow(10, 9) + 7;
+    // std::cout << std::endl;
     
     return (resultArr[m-1] % mod);
 }
