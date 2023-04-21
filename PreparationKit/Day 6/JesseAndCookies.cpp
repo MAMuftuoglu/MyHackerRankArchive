d31cf8d289e9b34852e7321b0d651a3a4a6f4c4c
Mehmet Arda Muftuoglu <arda.muftuoglu@wur.nl>
2023-04-21 12:23:39 +0200

    Create file

--- PreparationKit/Day 6/JesseAndCookies.cpp
+++ PreparationKit/Day 6/JesseAndCookies.cpp
@@ -1,11 +1,5 @@
 #include <bits/stdc++.h>
 
-using namespace std;
-
-string ltrim(const string &);
-string rtrim(const string &);
-vector<string> split(const string &);
-
 /*
  * Complete the 'cookies' function below.
  *
@@ -16,82 +10,28 @@ vector<string> split(const string &);
  */
 
 
-int cookies(int k, vector<int> A) {
+int cookies(int k, std::vector<int> A) {
+    int result = -1;
 
+
+
+    return result;
 }
 
 
-//Hacker Rank Functions
 int main()
 {
-    ofstream fout(getenv("OUTPUT_PATH"));
+    int n, k;
 
-    string first_multiple_input_temp;
-    getline(cin, first_multiple_input_temp);
+    std::cin >> n >> k;
 
-    vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+    std::vector<int> array(n);
 
-    int n = stoi(first_multiple_input[0]);
-
-    int k = stoi(first_multiple_input[1]);
-
-    string A_temp_temp;
-    getline(cin, A_temp_temp);
-
-    vector<string> A_temp = split(rtrim(A_temp_temp));
-
-    vector<int> A(n);
-
-    for (int i = 0; i < n; i++) {
-        int A_item = stoi(A_temp[i]);
-
-        A[i] = A_item;
+    for(int index = 0; index < n; index++) {
+        std::cin >> array[index];
     }
 
-    int result = cookies(k, A);
-
-    fout << result << "\n";
-
-    fout.close();
+    std::cout << cookies(k, array) << std::endl;
 
     return 0;
-}
-
-string ltrim(const string &str) {
-    string s(str);
-
-    s.erase(
-        s.begin(),
-        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
-    );
-
-    return s;
-}
-
-string rtrim(const string &str) {
-    string s(str);
-
-    s.erase(
-        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
-        s.end()
-    );
-
-    return s;
-}
-
-vector<string> split(const string &str) {
-    vector<string> tokens;
-
-    string::size_type start = 0;
-    string::size_type end = 0;
-
-    while ((end = str.find(" ", start)) != string::npos) {
-        tokens.push_back(str.substr(start, end - start));
-
-        start = end + 1;
-    }
-
-    tokens.push_back(str.substr(start));
-
-    return tokens;
 }
