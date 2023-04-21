6d5a020f28c7f34677e27c4b31862deea0bcf9a6
Mehmet Arda Muftuoglu <arda.muftuoglu@wur.nl>
2023-04-21 11:45:18 +0200

    Finalize the code, solve problem regarding modulo
    The problem about overflow solved by using unsigned long long
    While substracting unvalid formations result got below zero. Substraction
    changed for proper use of modulo.

--- PreparationKit/LegoBlockDP.cpp
+++ PreparationKit/LegoBlockDP.cpp
@@ -1,70 +1,71 @@
 #include <iostream>
 #include <cmath>
+#include <vector>
+#include <cstdint>
 
-unsigned long addMod(unsigned long a, unsigned long b, unsigned long mod) {
+typedef unsigned long long ull;
+
+ull addMod(ull a, ull b, ull mod)
+{
     return (a < (mod - b)) ? a + b : a - (mod - b);
 }
 
-unsigned long multiplyMod(unsigned long a, unsigned long b, unsigned long mod) {
-    unsigned int result = 0;
+ull powMod(ull a, ull b, ull mod)
+{
+    ull result = 1;
+    ull a_ = a % mod;
 
-    while(b > 0){
-        if(b & 1) result = addMod(result, a, mod);
-        a = addMod(a, a, mod);
+    while (b > 0)
+    {
+        if (b & 1)
+            result = result = result * a_ % mod;
+        a_ = a_ * a_ % mod;
         b >>= 1;
     }
 
     return result;
 }
 
-unsigned long powMod(unsigned long a, unsigned long b, unsigned long mod) {
-    unsigned long result = 1;
-
-    while(b > 0) {
-        if(b & 1) result = multiplyMod(result, a, mod);
-        a = multiplyMod(a, a, mod);
-        b >>= 1;
-    }
-
-    return result;
-}
-
-int legoBlocks(int n, int m) {
-    unsigned long * rowArr = new unsigned long[m];
+ull legoBlocks(ull n, ull m)
+{
+    std::vector<ull> rowArr(m + 1);
     rowArr[0] = 1;
-    int indexSum;
-    unsigned long mod = std::pow(10, 9) + 7;
-    
-    for(int index = 1; index < m; index++) {
+    ull indexSum;
+    ull mod = 1000000007;
+
+    for (int index = 1; index <= m; index++)
+    {
         indexSum = 0;
-        for(int blockSize = 1; blockSize <=4 && (index - blockSize) >= -1; blockSize++) {
-            if(index - blockSize == -1) addMod(indexSum, 1, mod);
-            else if (index - blockSize > -1) indexSum += rowArr[index - blockSize];
+        for (int blockSize = 1; blockSize <= 4 && (index - blockSize) >= 0; blockSize++)
+        {
+
+            indexSum = addMod(indexSum, rowArr[index - blockSize], mod);
         }
-        
-        rowArr[index] =  indexSum;
+
+        rowArr[index] = indexSum;
     }
-    
-    
-    unsigned long * resultArr = new unsigned long[m];
-    resultArr[0] = 1;
-    
-    for(int index = 1; index < m; index++) {
-        indexSum = std::pow(rowArr[index], n);
-        for(int leftCut = 0; leftCut < index; leftCut++) {
-            indexSum -= resultArr[leftCut] * std::pow(rowArr[index - leftCut - 1], n);
+
+    std::vector<ull> resultArr(m + 1);
+    resultArr[1] = 1;
+
+    for (int index = 2; index <= m; index++)
+    {
+        indexSum = powMod(rowArr[index], n, mod);
+        ull partToSubstract = 0;
+        for (int leftCut = 1; leftCut < index; leftCut++)
+        {
+            partToSubstract = resultArr[leftCut] * powMod(rowArr[index - leftCut], n, mod) % mod;
+            indexSum = (indexSum - partToSubstract + mod) % mod;
         }
         resultArr[index] = indexSum;
-        // std::cout << indexSum << " ";
     }
-    
-    // std::cout << std::endl;
-    
-    return (resultArr[m-1] % mod);
+
+    return resultArr[m];
 }
 
-int main() {
-    int n, m;
+int main()
+{
+    ull n, m;
 
     std::cin >> n >> m;
 
