ab17ff89a4a233c833442324cee26cefed2b90a1
Mehmet Arda Muftuoglu <arda.muftuoglu@wur.nl>
2023-04-24 12:11:11 +0200

    Made minHeap class so that is has better structure
    Before it had 2 errors in test cases, thus change it to a class

--- PreparationKit/Day 6/JesseAndCookies.cpp
+++ PreparationKit/Day 6/JesseAndCookies.cpp
@@ -9,75 +9,141 @@
  *  2. INTEGER_ARRAY A
  */
 
-void percolateDown(std::vector<int> &array, int hole, int size)
-{
-    int child, temp;
+/* ---------- Min Heap ---------- */
 
-    for (temp = array[hole]; (2 * hole + 1) < size; hole = child)
-    {
-        child = 2 * hole + 1;
-        if ((child != size - 1) && (array[child] > array[child + 1]))
-            child++;
-        if (array[child] < temp)
-            array[hole] = array[child];
-        else
-            break;
-    }
-    array[hole] = temp;
+class minHeap
+{
+public:
+    minHeap(const std::vector<int> &);
+
+    void insert(int);
+    int deleteMin();
+    int size() const;
+    int getMin() const;
+
+private:
+    std::vector<int> heap;
+
+    void percolateUp(int);
+    void percolateDown(int);
+    void heapify();
+};
+
+minHeap::minHeap(const std::vector<int> &arr) : heap(arr)
+{
+    heapify();    
 }
 
-int deleteMin(std::vector<int> &array, int &size)
+int minHeap::getMin() const {
+    return heap[0];
+}
+
+void minHeap::insert(int value)
 {
-    int min = array[0];
+    heap.push_back(value);
+    percolateUp(size() - 1);
+}
 
-    array[0] = array[size - 1];
-    size--;
-    percolateDown(array, 0, size);
+int minHeap::deleteMin()
+{
+    if (size() == 0)
+        throw "Heap is empty";
 
+    int min = heap[0];
+    heap[0] = heap[size() - 1];
+    heap.pop_back();
+    percolateDown(0);
     return min;
 }
 
-void insert(std::vector<int> &array, int item, int &size) {
-    int hole = size++;
+void minHeap::heapify()
+{
+    for (int i = size() / 2 - 1; i >= 0; --i)
+    {
+        percolateDown(i);
+    }
+}
 
-    for(; hole > 1 && item < array[hole / 2]; hole /= 2) {
-        array[hole] = array[hole / 2];
+void minHeap::percolateUp(int hole)
+{
+    int value = heap[hole];
+
+    while (hole > 0 && value < heap[(hole - 1) / 2])
+    {
+        heap[hole] = heap[(hole - 1) / 2];
+        hole = (hole - 1) / 2;
+    }
+    heap[hole] = value;
+}
+
+void minHeap::percolateDown(int hole)
+{
+    int value = heap[hole];
+    int child;
+
+    for (; 2 * hole + 1 < size(); hole = child)
+    {
+        child = 2 * hole + 1;
+        if (child + 1 < size() && heap[child + 1] < heap[child])
+            child++;
+
+        if (heap[child] < value)
+            heap[hole] = heap[child];
+        else
+            break;
     }
 
-    array[hole] = item;
+    heap[hole] = value;
 }
+
+int minHeap::size() const
+{
+    return heap.size();
+}
+
+/* ---------- Main Function ---------- */
 
 int cookies(int k, std::vector<int> A)
 {
+    minHeap heap(A);
+
     int result = 0;
 
-    int index, size = A.size();
-    for (index = (size - 1) / 2; index >= 0; index--)
-    {
-        percolateDown(A, index, size);
-    }
+    while(heap.getMin() < k && heap.size() > 1) {
+        int combine1 = heap.deleteMin();
+        int combine2 = heap.deleteMin();
 
-    while (A[0] < k && size > 1)
-    {
-        int combine1 = deleteMin(A, size), combine2 = deleteMin(A, size);
-        insert(A, combine1 + 2 * combine2, size);
+        int toInsert = combine1 + 2 * combine2;
+
+        heap.insert(toInsert);
         result++;
     }
 
-    return (A[0] >= k) ? result : -1;
+    return (heap.getMin() < k) ? -1 : result;
 }
 
 int main()
 {
     int n, k;
 
-    std::cin >> n >> k;
+    std::ifstream inputFile;
+    std::string fileName = "input.txt";
+    inputFile.open(fileName.c_str());
+    std::string line;
+    std::getline(inputFile, line);
+    std::stringstream lineReader(line);
+
+    lineReader >> n >> k;
+
+    std::getline(inputFile, line);
+
+    lineReader = std::stringstream(line);
 
     std::vector<int> array(n);
 
     for (int index = 0; index < n; index++)
     {
-        std::cin >> array[index];
+        lineReader >> array[index];
     }
 
     std::cout << cookies(k, array) << std::endl;
